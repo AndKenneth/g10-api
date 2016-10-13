e81e07cfc5d290c8b3a8a03c7e59538c6552b85b
Kenneth Anderson <kenneth@Kenneths-MacBook-Pro.local>
2016-10-14 11:21:19 +1300

    Refactor test data to outside of items model file.

--- src/models/category.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const categorySchema = {
-  name: String,
-};
-
-module.exports = mongoose.model('category', categorySchema);
---
+++ src/models/tests/item-test.js
@@ -0,0 +1,74 @@
+module.exports = [{
+  name: 'Sir Edmund Hillaries Ice Axe',
+  type: 'ecrm:E22_Man-Made_Object',
+  category: {
+    title: 'mountain climbing',
+  },
+  description: 'Ice axe used by Sir Edmund Hillary on first ascent of Everest, 29 May 1953\nmarkings: Claudius Simond / CHAMONIX MONT BLANC',
+  modelUrl: null,
+  museumIndex: 'collectionsonline-2016-08-30-1',
+  museumId: 'http://api.aucklandmuseum.com/id/humanhistory/object/671988',
+  contributor: 'Simond Chamonix',
+  dateAdded: 'Circa 1951',
+  isTaonga: false,
+  notes: 'This is the ice axe used by Sir Edmund Hillary during his ascent of Everest, in 1953. \"A few more whacks of the ice axe, a few weary steps, and we were on the summit of Everest (Ed Hillary, High Adventure, p208\n\nThis French Simond Chamonix ice axe acquired by Sir Edmund in 1951, and was used by him on the final ascent with Tensing Norgay, on 29 May 1953. An image on the Simond website shows Claudius Simond and Sir Edmund Hillary together at Chamonix.',
+  timePeriod: 'Elizabeth II (1952 -)/House of Windsor/English reign',
+  place: 'Nepal',
+}, {
+  name: 'Moa Skull',
+  category: {
+    title: 'Mammals',
+  },
+  description: 'A moa skull',
+  museumIndex: 'collectionsonline-2016-08-30-1',
+  museumId: 'http://api.aucklandmuseum.com/id/humanhistory/object/64501',
+  dateAdded: '02 Nov 1999',
+  isTaonga: false,
+  notes: 'The moa were nine species of flightless birds endemic to New Zealand. The two largest species, Dinornis robustus and Dinornis novaezelandiae, reached about 3.6 m in height with neck outstretched, and weighed about 230 kg.',
+  prefabName: 'moaskull',
+}, {
+  name: 'Hawkbill Sea Turtle',
+  category: {
+    title: 'Marine',
+  },
+  description: 'A sea turtle',
+  prefabName: 'turtle',
+  notes: 'The hawksbill sea turtle is a critically endangered sea turtle belonging to the family Cheloniidae. It is the only extant species in the genus Eretmochelys.',
+  dateAdded: '03 Sep 2003',
+}, {
+  name: 'Lion Skull',
+  category: {
+    title: 'Mammals',
+  },
+  description: 'A lion skull',
+  prefabName: 'lionskull',
+  notes: 'The lion is one of the big cats in the genus Panthera and a member of the family Felidae. The commonly used term African lion collectively denotes the several subspecies in Africa.',
+  dateAdded: '21 Jan 1993',
+}, {
+  name: 'Little blue penguin',
+  category: {
+    title: 'Marine',
+  },
+  description: 'A small blue penuin',
+  prefabName: 'penguin',
+  notes: 'The little penguin (Eudyptula minor) is the smallest species of penguin. It grows to an average of 33 cm (13 in) in height and 43 cm (17 in) in length, though specific measurements vary by subspecies.[2][3] It is found on the coastlines of southern Australia and New Zealand, with possible records from Chile.',
+  dateAdded: '22 Feb 2013',
+}, {
+  name: 'Maxim Mark 1 .303 Heavy Machine Gun',
+  category: {
+    title: 'Artefacts',
+  },
+  description: 'A large machine gun',
+  prefabName: 'gun',
+  notes: 'The Vickers machine gun or Vickers gun is a name primarily used to refer to the water-cooled .303 British (7.7 mm) machine gun produced by Vickers Limited, originally for the British Army. The machine gun typically required a six to eight-man team to operate: one fired, one fed the ammunition, the rest helped to carry the weapon, its ammunition and spare parts.[1] It was in service from before the First World War until the 1960s, with air-cooled versions of it on many Allied World War I fighter aircraft.',
+  dateAdded: '22 Feb 2013',
+},
+{
+  name: 'Maōri Pendant',
+  category: { title: 'Artefacts' },
+  description: 'a small pendant',
+  prefabName: 'pendant',
+  notes: 'Maori Bone and Greenstone Pendants carved in traditional designs carry a spiritual meaning as well',
+  dateAdded: '22 Feb 2013',
+},
+];
