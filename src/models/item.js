75f259466c893faa304c84904192cbb4dbe35402
Kenneth Anderson <kenneth@Kenneths-MacBook-Pro.local>
2016-10-14 00:05:35 +1300

    Added machine gun

--- src/models/item.js
+++ src/models/item.js
@@ -103,6 +103,14 @@ Item.remove({});
     notes: 'The little penguin (Eudyptula minor) is the smallest species of penguin. It grows to an average of 33 cm (13 in) in height and 43 cm (17 in) in length, though specific measurements vary by subspecies.[2][3] It is found on the coastlines of southern Australia and New Zealand, with possible records from Chile.',
     dateAdded: '22 Feb 2013',
   },
+  {
+    name: 'Maxim Mark 1 .303 Heavy Machine Gun',
+    category: { title: 'WW1' },
+    description: 'A large machine gun',
+    prefabName: 'penguin',
+    notes: 'The Vickers machine gun or Vickers gun is a name primarily used to refer to the water-cooled .303 British (7.7 mm) machine gun produced by Vickers Limited, originally for the British Army. The machine gun typically required a six to eight-man team to operate: one fired, one fed the ammunition, the rest helped to carry the weapon, its ammunition and spare parts.[1] It was in service from before the First World War until the 1960s, with air-cooled versions of it on many Allied World War I fighter aircraft.',
+    dateAdded: '22 Feb 2013',
+  },
 ].forEach((seedItem) => {
   Item.find({ name: seedItem.name }, (err, items) => {
     if (!items.length) {
