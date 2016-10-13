d2990feefaffc3d0daba345eaf1d69bf0030d57c
Kenneth Anderson <kenneth@Kenneths-MacBook-Pro.local>
2016-10-13 15:08:37 +1300

    Fixes category issue, capitalises classes

--- src/app.js
+++ src/app.js
@@ -6,27 +6,27 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/test');
 
-const item = require('./models/item.js');
-const category = require('./models/category.js');
+const Item = require('./models/Item.js');
+// const Category = require('./models/category.js');
 
 // Find a single item from the collection by name.
 app.get('/item/:name', (req, res) => {
-  item.findOne({ name: req.params.name }, (err, theItem) => res.send(theItem));
+  Item.findOne({ name: req.params.name }, (err, theItem) => res.send(theItem));
 });
 
 // List all items
 app.get('/items', (req, res) => {
-  item.find((err, items) => res.send({ items }));
+  Item.find((err, items) => res.send({ items }));
 });
 
 // Find item by category name
 app.get('/items/:category', (req, res) => {
-  item.find({ category }, (err, items) => res.send({ items }));
+  Item.find({ category: req.params.category }, (err, items) => res.send({ items }));
 });
 
 // Get all category names
 app.get('/categories', (req, res) => {
-  item.find({}, (err, items) => res.send({ categories: unique(items.map(i => i.category)) }));
+  Item.find({}, (err, items) => res.send({ categories: unique(items.map(i => i.category)) }));
 });
 
 app.use(express.static('public'));
